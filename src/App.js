20c375d4153dbb08305a237fc9ad7d739419b9a2
naseokju <lukas95@naver.com>
2022-11-10 15:11:04 +0900

    d-day add

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import moment from 'moment';
 
 export default function App() {
   const today = new Date();
-
   const [dateState, setDateState] = useState(new Date());
   const changeDate = e => {
     setDateState(e)
   }
+  let gap = dateState.getTime() - today.getTime();
+  let result = Math.ceil(gap / (1000 * 60 * 60 * 24));
   return (
     <>
       <Calendar
@@ -18,6 +19,7 @@ export default function App() {
       />
       <p>오늘 날짜는 {moment(today).format('yyyy-MM-DD')}입니다.</p> 
       <p>선택하신 날짜는 {moment(dateState).format('yyyy-MM-DD')}입니다.</p>
+      <p>D-day까지 {result}일 남았습니다.</p>
     </>
   )
 }
